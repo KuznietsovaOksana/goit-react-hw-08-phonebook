e6cc1bb01de19bd63fb4ea035e97badd9526940a
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-01-19 22:55:46 +0200

    try to fix gh-pages

--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
-          <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook/">
             <App />
-            <GlobalStyleComponent />
-          </ThemeProvider>
-        </BrowserRouter>
+          </BrowserRouter>
+          <GlobalStyleComponent />
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
